2fcf487752f93fcf83428ba8350ff1ce303ad671
Matheus Marques <matheuspadilha.dev@gmail.com>
2024-04-28 19:42:56 -0300

    first commit

---
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
+
+@Component({
+  selector: 'app-form-busca',
+  templateUrl: './form-busca.component.html',
+  styleUrls: ['./form-busca.component.scss']
+})
+export class FormBuscaComponent {
+
+  @Output() realizarBusca = new EventEmitter()
+
+  constructor( 
+    public formBuscaService : FormBuscaService) {}
+
+  buscar () {
+    const formBuscaValue = this.formBuscaService.formBusca.value;
+    this.realizarBusca.emit(formBuscaValue)
+  }
+}
